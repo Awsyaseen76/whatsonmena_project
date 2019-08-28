69fefc0ba1c382ad129eb1ba3c6cd1580f5302ab
Aws <Awsyaseen76@gmail.com>
2019-08-28 23:57:47 +0300

    remove backend

--- back/auth_api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from auth_api.models import Auth
-
-# Register your models here.
-admin.site.register(Auth)
--- back/auth_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . import models
-
-class AuthSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Auth
-        # fields = ('id', 'email', 'password', 'createdAt', 'updatedAt')
-        fields = '__all__'
